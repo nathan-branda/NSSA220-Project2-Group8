a396b9e956d9dc3c4d0c6e4c26371acd025e43ff
Nathan Branda <nab7450@rit.edu>
2026-04-20 18:55:46 -0400

    calculated avg_reply_delay

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -13,6 +13,8 @@ def compute(packets, host_ips) :
 		metrics[i]['req_exchanges'] = 0
 		metrics[i]['total_rtt'] = 0
 		metrics[i]['avg_rtt'] = 0
+		metrics[i]['total_reply_delay'] = 0
+		metrics[i]['received_requests'] = 0
 
 		for packet in packets[i]:
 			if packet['type'] == 'request' and packet['src'] == host_ips[i]:
@@ -29,20 +31,21 @@ def compute(packets, host_ips) :
 				metrics[i]['total_rtt'] += (response_time - float(packet['time'])) * 1000
 
 			if packet['type'] == 'reply' and packet['src'] == host_ips[i]:
-				metrics[i]['sent_replies'] += 1
-
-		for packet in packets[i]:
-			if packet['type'] == 'request' and packet['src'] != host_ips[i]:
+				for pack in packets[i]:
+					if pack['num'] == packet['assoc']:
+						metrics[i]['total_reply_delay'] += float(packet['time']) - float(pack['time'])
+						break
 				metrics[i]['received_requests'] += 1
-				metrics[i]['received_request_bytes'] += int(packet['len'])
-				metrics[i]['received_request_data'] += int(packet['len']) - 42
-			if packet['type'] == 'reply' and packet['src'] != host_ips[i]:
-				metrics[i]['received_replies'] += 1
+
+
+			if packet['type'] == 'reply' and packet['src'] == host_ips[i]:
+				metrics[i]['sent_replies'] += 1
 				
 	for node in metrics:
 		node['avg_rtt'] = node['total_rtt'] / node['req_exchanges']
 		node['request_throughput'] = node['sent_request_bytes'] / node['total_rtt'] # kB and milliseconds cancel out
 		node['request_goodput'] = node['sent_request_data'] / node['total_rtt']
+		node['avg_reply_delay'] = node['total_reply_delay'] / node['received_requests'] * 1000000
 
 	print(metrics)
 	print('called compute function in compute_metrics.py')
